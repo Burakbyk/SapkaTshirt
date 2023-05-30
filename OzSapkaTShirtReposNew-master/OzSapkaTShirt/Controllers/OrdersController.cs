cbdbab34752d1cdc9d91fcd32ec1993f1385eddd
Burakbyk <cp.burakbuyuk@gmail.com>
2023-05-30 19:36:32 +0300

    Fix 30.05.2023
    30.05.2023 19:36 Orders contrl and views

--- OzSapkaTShirtReposNew-master/OzSapkaTShirt/Controllers/OrdersController.cs
+++ OzSapkaTShirtReposNew-master/OzSapkaTShirt/Controllers/OrdersController.cs
@@ -27,18 +27,19 @@ namespace OzSapkaTShirt.Controllers
         {
             List<Order>? order;
             string userId = User.FindFirstValue(ClaimTypes.NameIdentifier);
-            order =  _context.Orders.Where(o => o.UserId == userId && o.Status > 0).Include(o=>o.OrderProducts).ThenInclude(op=>op.Product).ToList();
+            order =  _context.Orders.Where(o => o.UserId == userId && o.Status > 0).ToList();
             
         
             if (order==null)
             {
-                return NotFound();
+                return View();
             }
           
             return View(order);
         }
 
         // GET: Orders/Details/5
+        // Basket Sepet
         public async Task<IActionResult> Details()
         {
             string userId = User.FindFirstValue(ClaimTypes.NameIdentifier);
@@ -51,29 +52,7 @@ namespace OzSapkaTShirt.Controllers
             return View(order);
         }
 
-        // GET: Orders/Create
-        public IActionResult Create()
-        {
-            ViewData["UserId"] = new SelectList(_context.Users, "Id", "Id");
-            return View();
-        }
-
-        // POST: Orders/Create
-        // To protect from overposting attacks, enable the specific properties you want to bind to.
-        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Create([Bind("Id,UserId,OrderDate,TotalPrice")] Order order)
-        {
-            if (ModelState.IsValid)
-            {
-                _context.Add(order);
-                await _context.SaveChangesAsync();
-                return RedirectToAction(nameof(Index));
-            }
-            ViewData["UserId"] = new SelectList(_context.Users, "Id", "Id", order.UserId);
-            return View(order);
-        }
+      
 
         public async Task<IActionResult> Approve(long? id)
         {
@@ -83,6 +62,7 @@ namespace OzSapkaTShirt.Controllers
             }
 
             var order = await _context.Orders.FindAsync(id);
+            var orderProducts = _context.Orders.Where(o => o.Id == id).Include(o => o.OrderProducts).FirstOrDefault();
             if (order == null)
             {
                 return NotFound();
@@ -91,27 +71,42 @@ namespace OzSapkaTShirt.Controllers
             order.OrderDate = DateTime.Now;
             _context.Update(order);
             _context.SaveChanges();
+            if (order.OrderProducts!=null)
+            {
+                HttpContext.Response.Cookies.Append("totalQuantity", orderProducts.OrderProducts.Sum(q => q.Quantity*-1).ToString());
+            }
             return RedirectToAction("Index","Orders");
         }
 
-        public async Task<IActionResult> RemoveOrder(long? id)
+        public IActionResult RemoveOrder(long? id)
         {
+            List<Order>? orderList;
             Order? order;
+          
             string userId = User.FindFirstValue(ClaimTypes.NameIdentifier);
-            if (id == null || _context.Orders == null)
+
+            if (id != null || _context.Orders != null)
             {
-                return NotFound();
+                order = _context.Orders.Where(o => o.Id == id && o.UserId == userId).Include(o => o.OrderProducts).FirstOrDefault();
+                if (order != null)
+                {
+
+                    _context.Remove(order);
+                    _context.SaveChanges();
+                    orderList = _context.Orders.Where(o=>o.Status > 0).ToList();                   
+                    HttpContext.Response.Cookies.Append("totalQuantity", order.OrderProducts.Sum(q => q.Quantity * -1).ToString());
+                    return PartialView("OrderListPartial",orderList);
+                }
+
+                return PartialView("OrderListPartial", null);
+
+
             }
 
-            order =  _context.Orders.Where(o=>o.Id==id && o.UserId==userId).Include(o=>o.OrderProducts).FirstOrDefault();
-            if (order == null)
-            {
-                return NotFound();
-            }
-       
-            _context.Remove(order);
-            _context.SaveChanges();
-            return View(order);
+
+
+            return PartialView("OrderListPartial", null);
+
         }
 
 
@@ -119,113 +114,69 @@ namespace OzSapkaTShirt.Controllers
 
 
         // GET: Orders/Edit/5
-        public async Task<IActionResult> Edit(long? id)
+        public async Task<IActionResult> OrderProductsList(long? id)
         {
             Order? order;
-            OrderProduct? orderProduct;
-            Product? product = _context.Products.Find(id);
+            
+            
             string userId = User.FindFirstValue(ClaimTypes.NameIdentifier);
 
 
-            order = _context.Orders.Where(o => o.UserId == userId && o.Status > 0).Include(o => o.OrderProducts).FirstOrDefault();
+            order = _context.Orders.Where(o => o.Id == id && o.UserId==userId).Include(o => o.OrderProducts).ThenInclude(op=>op.Product).First();
+
             if (order != null && order.OrderProducts != null)
             {
-                foreach (var item in order.OrderProducts)
-                {
-                    if (item.Product == null)
-                    {
-                        item.Product = _context.Products.FirstOrDefault(q => q.Id == item.ProductId);
-                    }
-                }
+                return View(order);
             }
-            else
-            {
-                return NotFound();
-            }
+           
 
-
-
-            return View(order);
-        }
-
-        // POST: Orders/Edit/5
-        // To protect from overposting attacks, enable the specific properties you want to bind to.
-        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Edit(long id, [Bind("Id,UserId,OrderDate,TotalPrice")] Order order)
-        {
-            if (id != order.Id)
-            {
-                return NotFound();
-            }
-
-            if (ModelState.IsValid)
-            {
-                try
-                {
-                  
-                    _context.Update(order);
-                    await _context.SaveChangesAsync();
-                }
-                catch (DbUpdateConcurrencyException)
-                {
-                    if (!OrderExists(order.Id))
-                    {
-                        return NotFound();
-                    }
-                    else
-                    {
-                        throw;
-                    }
-                }
-                return RedirectToAction(nameof(Index));
-            }
-            ViewData["UserId"] = new SelectList(_context.Users, "Id", "Id", order.UserId);
-            return View(order);
-        }
-
-        // GET: Orders/Delete/5
-        public async Task<IActionResult> Delete(long? id)
-        {
-            if (id == null || _context.Orders == null)
-            {
-                return NotFound();
-            }
-
-            var order = await _context.Orders
-                .Include(o => o.User)
-                .FirstOrDefaultAsync(m => m.Id == id);
-            if (order == null)
-            {
-                return NotFound();
-            }
-
-            return View(order);
-        }
-
-        // POST: Orders/Delete/5
-        [HttpPost, ActionName("Delete")]
-        [ValidateAntiForgeryToken]
-        public async Task<IActionResult> DeleteConfirmed(long id)
-        {
-            if (_context.Orders == null)
-            {
-                return Problem("Entity set 'ApplicationContext.Orders'  is null.");
-            }
-            var order = await _context.Orders.FindAsync(id);
-            if (order != null)
-            {
-                _context.Orders.Remove(order);
-            }
+                return View();
             
-            await _context.SaveChangesAsync();
-            return RedirectToAction(nameof(Index));
+
+
+          
         }
 
-        private bool OrderExists(long id)
+       
+   
+      
+        public IActionResult DeleteOrder(long? id)
         {
-          return (_context.Orders?.Any(e => e.Id == id)).GetValueOrDefault();
+           Order? order;
+
+           string userId = User.FindFirstValue(ClaimTypes.NameIdentifier);
+
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+
+        order = _context.Orders.Where(o => o.Id == id && o.UserId == userId).Include(o => o.OrderProducts).FirstOrDefault();
+        if(order != null )
+         {
+                if (order.Status < 3)
+                {
+                    _context.Remove(order);
+
+                    _context.SaveChanges();
+
+                    return RedirectToAction("Index", "Orders");
+                }
+
+                return NotFound();
+
+         }
+       
+        
+
+             return NotFound();
+            
+            
+           
         }
+
+       
+      
     }
 }
